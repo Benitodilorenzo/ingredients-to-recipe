7f466c024e78b51c10c4094f243c448c6708e9e0
v1kt0rz <121103628+v1kt0rz@users.noreply.github.com>
2023-03-15 16:47:34 +0100

    Update About the app.py

--- pages/About the app.py
+++ pages/About the app.py
@@ -23,8 +23,8 @@ add_bg_from_url()
 
 st.markdown('###### This app is the final project we developed over the last few days at Le Wagon\'s data science bootcamp in March 2023.')
 
-st.markdown("- Our app recognizes ingredients on uploaded images using a powerful algorithm called yolov7. We trained this Convolutional Neural Network on a dataset of 10,000 images featuring 131 categories of ingredients (fruits, vegetables, eggs).")
-st.markdown("- We created a FastAPI to run the object detection on the uploaded image and return the objects and their confidence levels.")
-st.markdown("- We've also integrated Restegourmet's API, which provides a wide range of recipes.")
+st.markdown("- Our app recognizes ingredients on uploaded images using a powerful algorithm called yolov7. We trained this Convolutional Neural Network on a dataset of 20,000 images featuring 24 categories of ingredients (fruits & vegetables).")
+st.markdown("- We created a FastAPI to run the object detection on the uploaded image and return objects and their confidence levels.")
+st.markdown("- We've also integrated Restegourmet's API, which provides a wide range of recipes in German.")
 
 st.markdown("###### We\'d like to thank Restegourmet for their cooperation and Le Wagon\'s teachers for their guidance and endless patience. We\'re proud to present this app as the culmination of our efforts, and we hope that you find it useful and enjoyable.")
